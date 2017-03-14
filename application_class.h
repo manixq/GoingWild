cbf9f877008c163e7002f05651a14910e9eea998
Pawel M <obramanix@gmail.com>
2017-03-14 18:13:33 +0100

    added BitmapClass

---
+++ application_class.h
@@ -0,0 +1,11 @@
+#ifndef  _APPLICATION_CLASS_H_
+#define _APPLICATION_CLASS_H_
+
+#include "input_class.h"
+#include "d3d_class.h"
+#include "camera_class.h"
+#include "model_class.h"
+#include "texture_shader_class.h"
+#include "light_shader_class.h"
+#include "light_class.h"
+#endif
